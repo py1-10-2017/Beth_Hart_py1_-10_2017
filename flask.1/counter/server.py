07ae6b2a537097704b73dcc450b107a52a41ad33
Beth Hart <bethhart27@gmail.com>
2017-10-09 20:39:33 +0000

    rework session logic and debug reset button

--- flask.1/counter/server.py
+++ flask.1/counter/server.py
@@ -4,17 +4,23 @@ app = Flask(__name__)
 app.secret_key = "mikaylamommy"
 
 @app.route('/')
-def counter():
+def start():
+    if session:
+        session['counter'] = session['counter']
+    else:
+        session["counter"]=1
     return render_template('index.html')
 
 @app.route("/add", methods=["POST"])
 def count():
      session['counter'] += 2
+    #  return render_template("index.html")
      return redirect("/")
 
+#reset not working
 @app.route("/reset", methods=["POST"])
 def reset():
     session['counter'] = 0
+    # return render_template("index.html")
     return redirect("/")
-    
 app.run(host="0.0.0.0", port=int("8080"), debug=True)
